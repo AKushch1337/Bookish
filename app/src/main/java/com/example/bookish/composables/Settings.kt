fc1d7d07c048b9b3b5db03303309f032ec07ab0a
Artem Kushch <artemkusch2@gmail.com>
2023-05-31 17:09:08 +0300

    Add three screens for all main UI components with placeholder text

---
+++ app/src/main/java/com/example/bookish/composables/Settings.kt
@@ -0,0 +1,29 @@
+package com.example.bookish.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SettingsScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Settings",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+    }
+}

accc9edd4bff19a69892c492fd70d8db93db15f4
Artem Kushch <artemkusch2@gmail.com>
2023-06-04 01:03:39 +0300

    Optimize imports

--- app/src/main/java/com/example/bookish/MainActivity.kt
+++ app/src/main/java/com/example/bookish/MainActivity.kt
@@ -3,10 +3,6 @@ package com.example.bookish
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.bookish.composables.MainScreen
 import com.example.bookish.ui.theme.BookishTheme
 

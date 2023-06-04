ccbb2afdaa0462062b3c43d52d08c951b1599014
Artem Kushch <artemkusch2@gmail.com>
2023-06-04 14:33:43 +0300

    Implement UI for books, add placeholder books and cover for now into Library

--- app/src/main/java/com/example/bookish/composables/screens/Library.kt
+++ app/src/main/java/com/example/bookish/composables/screens/Library.kt
@@ -2,7 +2,13 @@ package com.example.bookish.composables.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -10,22 +16,32 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.bookish.R
+import com.example.bookish.composables.components.ItemBookList
 
 @Composable
 fun LibraryScreen() {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Red),
-        contentAlignment = Alignment.Center
+            .background(Color.White),
     ) {
-        Text(
-            text = stringResource(R.string.library),
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.White
-        )
+        Column(modifier = Modifier
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())) {
+            repeat(30) {
+                ItemBookList()
+                Spacer(modifier = Modifier.height(10.dp))
+            }
+        }
     }
+}
+
+@Preview
+@Composable
+fun LibraryScreenPreview() {
+    LibraryScreen()
 }
